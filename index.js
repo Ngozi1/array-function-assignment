44ff5a8ad9b468c4205436843cca873dcff38c5e
Ngozi1 <103275987+Ngozi1@users.noreply.github.com>
2023-03-06 06:28:41 -0800

    my commit

--- index.js
+++ index.js
@@ -32,8 +32,19 @@ console.log(languages)
 
 // Question 3
 
-let fruit = ['apple', 'mango', 'banana'];
-  function changeFruit(fruit) {
-     fruit [2] = 'orange';
-     return fruit;
-}
+// let fruit = ['apple', 'mango', 'banana'];
+// 		function changeFruit( fruit ) {
+//     			fruit[2] = "orange";
+//     			return fruit;
+// 		}
+
+//    ANSWER
+
+// After calling the function createFruit the values of fruit will be ['apple', 'mango', 'orange']
+//  since it is index 2 and remember our index starts from 0 so it will change the 3rd item
+
+
+
+// QUESTION 4
+
+
