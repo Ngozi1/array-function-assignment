8dcc2ff72318216bb9fef96dba3c6a0b44512229
Ngozi1 <103275987+Ngozi1@users.noreply.github.com>
2023-03-08 05:59:50 -0800

    my commit

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 // // QUESTION 1
 
+//  Mutation in context of JavaScript arrays is basically changing the array itself instead of 
+// returning a new array with the new changes whereas Non-Mutation is returning a whole 
+// new array which has all the changes.
+
+// ARRAY METHODS THAT FALLS UNDER MUTATION 
+
+//  array.copyWithin(), 
+// array.fill(), 
+// array.pop(), 
+// array.push(), 
+// array.reverse(), 
+// array.shift(), 
+// array.sort(), 
+// array.splice(),
+//  array.unshift()
+
+ // ARRAY METHODS THAT FALLS UNDER NON-MUTATION
+
+//  array.concat(),
+//  array.includes(),
+//  array.indexOf,
+//  array.join(),
+//  array.lastIndexOf,
+//  array.slice(),
+//  array.toString() 
+//  array.toLocaleString()
+
+
 
 
 
@@ -32,19 +60,38 @@ console.log(languages)
 
 // Question 3
 
-// let fruit = ['apple', 'mango', 'banana'];
-// 		function changeFruit( fruit ) {
-//     			fruit[2] = "orange";
-//     			return fruit;
-// 		}
+let fruit = ['apple', 'mango', 'banana'];
+		function changeFruit( fruit ) {
+    			fruit[2] = "orange";
+    			return fruit;
+		}
+
+        console.log(changeFruit( fruit ))
+
+
 
 //    ANSWER
 
 // After calling the function createFruit the values of fruit will be ['apple', 'mango', 'orange']
-//  since it is index 2 and remember our index starts from 0 so it will change the 3rd item
+//  since it is index 2 and remember our index starts from 0 so it will change the 3rd item which is banana
 
 
 
 // QUESTION 4
 
+
+function maxFromArray(numbers) {
+    let max = numbers[0];                      
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] > max) {                
+            max = numbers[i];
+        }
+    }
+    return max;                                
+}
+
+const numbers = [19, 30, 1000, 5, 1, 67];
+console.log("The maximum value is" + " " + maxFromArray(numbers) );
+
+// QUESTION 5
 
